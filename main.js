56d65a39a81db471b795c4e2c66a3e618817c813
Manish Badsara <54879226+ManishBadsara@users.noreply.github.com>
2020-02-21 14:55:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function zoomin()
+{
+	document.getElementById('mainpic').style.width="450px";
+	document.getElementById('mainpic').style.height="280px";
+}
+
+function zoomout()
+{
+	document.getElementById('mainpic').style.width="500px";
+	document.getElementById('mainpic').style.height="300px";
+}
